f0f0ac591fa39c2a4a559529d2c8a7e0cf816b8c
Thomaspls-unity <152604731+Thomaspls-unity@users.noreply.github.com>
2024-02-05 14:03:45 +0000

    Lab 10.1 Submission
    Created a shooting function with a raycast so that the object shooting can detect what it is aimed at.

---
+++ Assets/UnityTechnologies/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour
+{
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+
+    private AudioSource gunAudio;
+
+    private LineRenderer laserLine;
+
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0));
+
+        if (Input.GetButtonDown ("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+        }
+
+        RaycastHit hit;
+
+        laserLine.SetPosition(0, gunEnd.position);
+
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+        {
+            laserLine.SetPosition (1, hit.point);
+        }
+        else
+        {
+            laserLine.SetPosition(1, fpsCam.transform.forward * weaponRange);
+        }
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+
+        yield return shotDuration;
+
+        laserLine.enabled = false;
+    }
+}
